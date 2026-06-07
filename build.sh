ae6e4ce3e45c89b18b6500193c13a6e52ce307f8
AdityaKumarPhy <adityaKumar458959@protonmail.com>
2026-06-07 17:40:34 +0530

    Fix Quarto extraction folder

--- build.sh
+++ build.sh
@@ -5,10 +5,11 @@ echo "Downloading Quarto..."
 curl -LO https://github.com/quarto-dev/quarto-cli/releases/download/v1.4.554/quarto-1.4.554-linux-amd64.tar.gz
 
 echo "Extracting Quarto..."
-tar -zxvf quarto-1.4.554-linux-amd64.tar.gz
+mkdir -p _quarto_install
+tar -zxvf quarto-1.4.554-linux-amd64.tar.gz -C _quarto_install --strip-components=1
 
 # Add Quarto to the path
-export PATH="$(pwd)/quarto-1.4.554/bin:$PATH"
+export PATH="$(pwd)/_quarto_install/bin:$PATH"
 
 # Render the Quarto website
 echo "Rendering Quarto website..."
